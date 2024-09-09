37da6b0404906acd4431cba4e8a5e5c330d06acc
Percy Mudzinga <percy@betterdesigndigital.com>
2024-09-09 21:27:16 +0200

    Updated code

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,6 @@ app.post("/send-whatsapp-message", async (req, res) => {
     } else {
       console.log("sending message");
       const data = { message, phoneNumber: toNumber };
-      console.log("Data before sending", data);
       await sendNotification(data);
       await prisma.message.create({
         data: {
@@ -203,18 +202,17 @@ const sendMessageToFlow = async (data: {
 };
 
 const sendNotification = async (data: any) => {
-  console.log(data);
-  // const config: AxiosRequestConfig = {
-  //   headers: {
-  //     "Content-Type": "application/json", // Set the Content-Type header
-  //   },
-  // };
-  // var response = await axios.post(
-  //   `${process.env.APP_BASE_URL}/api/sendNotification`,
-  //   data,
-  //   config
-  // );
-  // console.log(response.data);
+  const config: AxiosRequestConfig = {
+    headers: {
+      "Content-Type": "application/json", // Set the Content-Type header
+    },
+  };
+  var response = await axios.post(
+    `${process.env.APP_BASE_URL}/api/sendNotification`,
+    data,
+    config
+  );
+  console.log(response.data);
 };
 
 const subtractDateTimeInMinutes = (date1: Date, date2: Date): number => {
